e87a103d99e96edf3b94363f9556bac55c852ba4
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-08-11 23:12:07 +0900

    Add ImutableObject

---
+++ src/main/java/ImutableObject/JpaMain.java
@@ -0,0 +1,34 @@
+package ImutableObject;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory EMF = Persistence.createEntityManagerFactory("hello");
+        EntityManager EM = EMF.createEntityManager();
+        EntityTransaction ET = EM.getTransaction();
+
+        ET.begin();
+        Member2 member = new Member2();
+        member.setName("Junseok");
+        member.setAddress(new Address("city", "street", "zipcode"));
+
+        member.setPeriod(new Period());
+
+        //값타입의 세터를 지움으로써 참조에의한 사이드이펙트를 예방할수있다.
+        //대신, 값 변경이 안되니 변경할떄는 통으로 새로 만들어야함.
+
+        member.setAddress(new Address("city2", "street2", "zipcode2"));
+
+        EM.persist(member);
+
+        ET.commit();
+        EM.close();
+        EMF.close();
+    }
+}
---
+++ src/main/java/ImutableObject/Member2.java
@@ -0,0 +1,52 @@
+package ImutableObject;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member2 {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @Embedded
+    private Address address;
+    
+    @Embedded
+    private Period period;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Period getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Period period) {
+        this.period = period;
+    }
+}
